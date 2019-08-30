1d6871f44771ef1937a7200b47966af79629bbb6
1909WDFNY <54694024+1909WDFNY@users.noreply.github.com>
2019-08-29 22:29:14 -0400

    Create simulateBattle_GraceH.js

---
+++ 04-constructor-functions/simulate-battle/simulateBattle_GraceH.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars, no-throw-literal*/
+
+const Pokemon = function(name, hp, atkBonus) {
+    this.name = name;
+    this.health = hp;
+    this.attackBonus = atkBonus;
+};
+
+Pokemon.prototype.biteAttack = function() {
+    return this.attackBonus + 2;    
+};
+
+Pokemon.prototype.isDefeated = function() {
+    if (this.health >= 1) return false;
+    return true;
+};
+
+//using recursion to continue the battle until one pokemon reaches 0 HP
+const simulateBattle = (pokemon1, pokemon2, firstAttacker) => {
+    let attacker = (firstAttacker === pokemon1.name) ? pokemon1 : pokemon2;
+    let opponent = (firstAttacker === pokemon1.name) ? pokemon2 : pokemon1;
+    
+    if (attacker.isDefeated()) return `${opponent.name} Wins!`;
+    opponent.health -= attacker.biteAttack();
+    if (opponent.isDefeated()) return `${attacker.name} Wins!`;
+    attacker.health -= opponent.biteAttack();
+    return simulateBattle(attacker, opponent, firstAttacker);
+};
